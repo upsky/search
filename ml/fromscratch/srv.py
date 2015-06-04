934968fb0d43ca5d651c99ef727923eb3121ffad
devidark <devidark@gmail.com>
2015-06-04 20:06:13 +0300

    ml: very simple server and ml

--- ml/fromscratch/srv.py
+++ ml/fromscratch/srv.py
@@ -3,27 +3,54 @@
 
 import os
 import sys
+import urlparse
 import json
 
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from SocketServer   import ThreadingMixIn
 
+from testml import Classifier
 
+#-------------------------------------------------------------------------------
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
-    pass
+    def load_ml(self, json_file):
+        self.cls = Classifier()
+        self.cls.train_from_file( json_file )
+        print >> sys.stderr, "Trained on %d docs" % self.cls.loaded_docs
 
+#-------------------------------------------------------------------------------
 class HttpHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
 
-        # self.path .. to be continued at home ..
+        pp = urlparse.urlparse( self.path )
+        mq = urlparse.parse_qs( pp.query )
+        q = mq.get('q', [])
+        q = q[0] if len(q) > 0 else ''
 
-        self.wfile.write("my response: %s" % self.path)
+        cat_id = self.server.cls.predict( q )
 
+        obj = dict()
+        obj['category'] = self.server.cls.cat_id2name( cat_id )
 
+        resp = json.dumps(obj, ensure_ascii=False)
+        resp = resp.encode('utf-8')
+
+        self.wfile.write( resp )
+
+#-------------------------------------------------------------------------------
 if __name__ == '__main__':
-    srv = ThreadedHTTPServer(('localhost', 2222), HttpHandler)
+    if len(sys.argv) < 2:
+        print >> sys.stderr, "Usage:\n  " + sys.argv[0] + "  <json_scan_file>\n"
+        sys.exit(1)
+
+    json_file = sys.argv[1]
+
+    srv = ThreadedHTTPServer(('0.0.0.0', 2222), HttpHandler)
+
+    srv.load_ml(json_file)
+
     srv.allow_reuse_address = True
     srv.serve_forever()
--- ml/fromscratch/testml.py
+++ ml/fromscratch/testml.py
@@ -24,161 +24,162 @@ import random
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import SGDClassifier
+# from sklearn import svm
 
-#-------------------------------------------------------------------------------
-# parse argv
-if len(sys.argv) < 2:
-    print >> sys.stderr, "Usage:\n  %s <scan_json_file> [keyboard]" % sys.argv[0]
-    sys.exit(1)
-
-scan_json_file = sys.argv[1]
-
-test_from_keyboard = False
-if len(sys.argv) > 2 and sys.argv[2] == 'keyboard':
-    test_from_keyboard = True
-# test_json_file = sys.argv[2]
-
-#-------------------------------------------------------------------------------
-cats = dict()
-n2cat = dict()
-cat_n = 0
-
-def LoadData(json_file, out_data):
-    global cats
-    global n2cat
-    global cat_n
-
-    for line in open(json_file):
-        line = line.strip()
-        rec = json.loads( line )
-
-        cat_name = rec['cat']
-        sub_cat_name = rec['sub_cat']
-        title = rec['title']
-        desc = rec['desc']
-
-        '''sss = u"%s\t%s\t%s" % (cat_name, title, desc)
-        print sss.encode('utf-8')
-        continue'''
-
-        c = cat_name
-        cat_id = cats.get(c, None)
-
-        if cat_id == None:
-            cats[c] = cat_n
-            n2cat[cat_n] = c
-            cat_id = cat_n
-            cat_n += 1
-
-        out_data.append( (title, cat_id) )
-        #out_data.append( (desc, cat_id) )
-
-def CatId2Name(id):
-    global n2cat
-    return n2cat.get(id, '<UNKNOWN>')
-
-
-#-------------------------------------------------------------------------------
-# read
-data = list()
-LoadData(scan_json_file, data)
-print >> sys.stderr, "Loaded %d documents" % len(data)
-
-# перемешиваем выборку и подготавливаем из неё обучающее и тестовое множества
-random.seed()
-random.shuffle( data )
-
-if test_from_keyboard:
-    train_data = data
-    test_data = []
-else:
-    r = len(data)*2/3
-    train_data = data[:r]
-    test_data = data[r:]
-
-train_docs   = [d for (d, c) in train_data]
-train_target = [c for (d, c) in train_data]
-
-test_docs   = [d for (d, c) in test_data]
-test_target = [c for (d, c) in test_data]
-
-# парсим
-# v = CountVectorizer()
-# v = TfidfVectorizer(ngram_range=(1,1))
-# v = TfidfVectorizer(ngram_range=(1,2))
-#-----------------------------------------------------------
 import nltk.stem
+
+#-------------------------------------------------------------------------------
 russian_stemmer = nltk.stem.SnowballStemmer('russian')
 
 class StemmedTfidfVectorizer( TfidfVectorizer ):
     def build_analyzer(self):
         analyzer = super(TfidfVectorizer, self).build_analyzer()
+        global russian_stemmer
         return lambda doc: (russian_stemmer.stem(word) for word in analyzer(doc))
-#-----------------------------------------------------------
-
-v = StemmedTfidfVectorizer(min_df=1, ngram_range=(1,2))
-train_features = v.fit_transform(train_docs)
-
-# train
-print >> sys.stderr, "Learning..."
-from sklearn import svm
-from sklearn.linear_model import SGDClassifier
-
-# cls = svm.SVC()
-cls = SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=5, random_state=42)
-
-print >> sys.stderr, cls
-cls.fit(train_features, train_target)
-print >> sys.stderr, " - done"
 
 #-------------------------------------------------------------------------------
-# test
-if test_from_keyboard:
-    # predict from keyboard
-    print "And now let's predict from keyboard.\nType any query in russian language:"
-    while (True):
-        line = raw_input()
-        test_features = v.transform([line])
-        pred_t = cls.predict(test_features)
-        pred_t = pred_t[0]
-        print "\t%d=%s" % (pred_t, CatId2Name(pred_t))
-else:
-    # predict from the same collection
-    print >> sys.stderr, "Testing..."
+class Classifier:
+    def __init__(self):
+        self.cats = dict()
+        self.n2cat = dict()
+        self.cat_n = 0
 
-    # test_docs = list()
-    # test_target = list()
-    # LoadData(test_json_file, test_docs, test_target)
-    # print >> sys.stderr, "Loaded %d test documents" % len(test_docs) '''
+        self.vectorizer = StemmedTfidfVectorizer(min_df=1, ngram_range=(1,2))
+        self.classifier = None
+        self.loaded_docs = 0
 
-    test_predicted = list()
+    #-------------------------------------------------------
+    def train_from_file(self, json_file):
+        (docs, cats) = self.load_data(json_file)
+        self.train(docs, cats)
+        self.loaded_docs = len(docs)
 
-    eq = 0
-    for i in xrange( len(test_docs) ):
-        d = test_docs[i]
-        t = test_target[i]
+    #-------------------------------------------------------
+    def load_data(self, json_file):
+        docs = list()
+        cats = list()
 
-        test_features = v.transform([d])
-        pred_t = cls.predict(test_features)
+        for line in open(json_file):
+            line = line.strip()
+            rec = json.loads( line )
 
-        pred_t = pred_t[0]
+            cat_name = rec['cat']
+            sub_cat_name = rec['sub_cat']
+            title = rec['title']
+            desc = rec['desc']
 
-        label = "BAD"
-        if t == pred_t:
-            eq += 1
-            label = "OK"
+            c = cat_name
+            cat_id = self.cats.get(c, None)
 
-        prn = u"%d\t%s\t'%s'\texpected/predicted: %d=%s / %d=%s" % (i, label, d, t, CatId2Name(t), pred_t, CatId2Name(pred_t))
-        print prn.encode('utf-8')
+            if cat_id == None:
+                self.cats[c] = self.cat_n
+                self.n2cat[self.cat_n] = c
+                cat_id = self.cat_n
+                self.cat_n += 1
 
-        test_predicted.append( pred_t )
+            docs.append( title )    # desc )
+            cats.append( cat_id )
 
-    # show metrics
-    # accuracy = float(eq) / float(len(test_docs))
-    # print "Total: %d, equal: %d, accuracy: %f" % (len(test_docs), eq, accuracy)
-    from sklearn import metrics
+        return (docs, cats)
 
-    cat_names = [CatId2Name(x) for x in xrange(cat_n)]
-    s = metrics.classification_report(test_target, test_predicted, target_names=cat_names)
-    print
-    print s.encode('utf-8')
+    #-------------------------------------------------------
+    def cat_id2name(self, id):
+        return self.n2cat.get(id, '<UNKNOWN>')
+
+    #-------------------------------------------------------
+    def train(self, train_docs, train_target):
+        print >> sys.stderr, "Learning..."
+        train_features = self.vectorizer.fit_transform(train_docs)
+
+        # classifier = svm.SVC()
+        self.classifier = SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=5, random_state=42)
+        print >> sys.stderr, self.classifier
+
+        self.classifier.fit(train_features, train_target)
+        print >> sys.stderr, " - done"
+
+    #-------------------------------------------------------
+    def predict(self, text):
+        test_features = self.vectorizer.transform([text])
+        p = self.classifier.predict(test_features)
+        return p[0]
+
+
+#-------------------------------------------------------------------------------
+# COMMAND LINE CASE
+#-------------------------------------------------------------------------------
+if __name__ == '__main__':
+    # parse argv
+    if len(sys.argv) < 2:
+        print >> sys.stderr, "Usage:\n  %s <scan_json_file> [keyboard]" % sys.argv[0]
+        sys.exit(1)
+
+    json_file = sys.argv[1]
+    test_from_keyboard = (len(sys.argv) > 2 and sys.argv[2] == 'keyboard')
+
+    cls = Classifier()
+    (docs, cats) = cls.load_data(json_file)
+    print >> sys.stderr, "Loaded %d documents" % len(docs)
+
+    # перемешиваем выборку и подготавливаем из неё обучающее и тестовое множества
+    data = [(d, c) for (d, c) in zip(docs, cats)]
+    random.seed()
+    random.shuffle( data )
+
+    if test_from_keyboard:
+        train_data = data
+        test_data = []
+    else:
+        r = len(data)*2/3
+        train_data = data[:r]
+        test_data = data[r:]
+
+    train_docs   = [d for (d,c) in train_data]
+    train_target = [c for (d,c) in train_data]
+    test_docs   = [d for (d,c) in test_data]
+    test_target = [c for (d,c) in test_data]
+
+    cls.train(train_docs, train_target)
+
+    # test
+    if test_from_keyboard:
+        # predict from keyboard
+        print "And now let's predict from keyboard.\nType any query in russian language:"
+        while (True):
+            line = raw_input()
+            cat_id = cls.predict(line)
+            print "\t%d=%s" % (cat_id, cls.cat_id2name(cat_id))
+    else:
+        # predict from the same collection
+        print >> sys.stderr, "Testing..."
+
+        test_predicted = list()
+
+        eq = 0
+        for i in xrange( len(test_docs) ):
+            d = test_docs[i]
+            t = test_target[i]
+
+            print d
+            cat_id = cls.predict(d)
+
+            label = "BAD"
+            if t == cat_id:
+                eq += 1
+                label = "OK"
+
+            prn = u"%d\t%s\t'%s'\texpected/predicted: %d=%s / %d=%s" % (i, label, d, t, cls.cat_id2name(t), cat_id, cls.cat_id2name(cat_id))
+            print prn.encode('utf-8')
+
+            test_predicted.append( cat_id )
+
+        # show metrics
+        # accuracy = float(eq) / float(len(test_docs))
+        # print "Total: %d, equal: %d, accuracy: %f" % (len(test_docs), eq, accuracy)
+        from sklearn import metrics
+
+        cat_names = [cls.cat_id2name(x) for x in xrange(cls.cat_n)]
+        s = metrics.classification_report(test_target, test_predicted, target_names=cat_names)
+        print
+        print s.encode('utf-8')
