c2a6cb0e5c9c01e2bce1fab54b4334ccad72108b
devidark <devidark@gmail.com>
2015-06-16 15:01:54 +0300

    classifier fixes and improvement; sealing from-scratch as POCofPOC ;); server returns cats hier

--- ml/fromscratch/srv.py
+++ ml/fromscratch/srv.py
@@ -11,6 +11,8 @@ from SocketServer   import ThreadingMixIn
 
 from testml import Classifier
 
+kBeautifyJson = True
+
 #-------------------------------------------------------------------------------
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     def load_ml(self, json_file):
@@ -18,51 +20,65 @@ class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
         self.cls.train_from_file( json_file )
         print >> sys.stderr, "Trained on %d docs" % self.cls.loaded_docs
 
+    def load_hierarchy(self, hier_file):
+        self.cats_hier = json.load( open(hier_file) )
+
 #-------------------------------------------------------------------------------
 class HttpHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-type", "text/plain;charset=utf-8")
         self.end_headers()
 
         pp = urlparse.urlparse( self.path )
-        mq = urlparse.parse_qs( pp.query )
-        q = mq.get('q', [])
-        q = q[0] if len(q) > 0 else ''
-
-        if len(q) > 3:
-            cat_id = self.server.cls.predict( q )
-        else:
-            cat_id = -1
+        qmap = urlparse.parse_qs( pp.query )
 
         obj = dict()
-        obj['category'] = self.server.cls.cat_id2name( cat_id )
 
-        try:
-            log = 'QUERY\t%s\t%s' % (q, obj['category'].encode('utf-8'))
-            print >> sys.stderr, log
-        except Exception, e:
-            print >> sys.stderr, "Log exc: " + str(e)
+        if 'get' in qmap:
+            what = qmap['get']
+            what = what[0] if len(what) == 1 else ''
+            if what == 'categories':
+                obj = self.server.cats_hier
+        elif 'q' in qmap:
+            q = qmap.get('q', [])
+            q = q[0] if len(q) > 0 else ''
 
-        resp = json.dumps(obj, ensure_ascii=False)
+            if len(q) > 3:
+                cat_id = self.server.cls.predict( q )
+            else:
+                cat_id = -1
+
+            obj['category'] = self.server.cls.cat_id2name( cat_id )
+
+            try:
+                log = 'QUERY\t%s\t%s' % (q, obj['category'].encode('utf-8'))
+                print >> sys.stderr, log
+            except Exception, e:
+                print >> sys.stderr, "Log exc: " + str(e)
+
+        indent = None if not kBeautifyJson else 2
+        resp = json.dumps(obj, ensure_ascii=False, indent=indent, sort_keys=True )
         resp = resp.encode('utf-8')
 
         self.wfile.write( resp )
 
 #-------------------------------------------------------------------------------
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print >> sys.stderr, "Usage:\n  " + sys.argv[0] + "  <json_scan_file>\n"
+    if len(sys.argv) < 3:
+        print >> sys.stderr, "Usage:\n  " + sys.argv[0] + "  <scan_file.json>  <categories_hierarchy.json>\n"
         sys.exit(1)
 
     host = '0.0.0.0'
     port = 2222
 
     json_file = sys.argv[1]
+    hier_file = sys.argv[2]
 
     srv = ThreadedHTTPServer((host, port), HttpHandler)
 
     srv.load_ml(json_file)
+    srv.load_hierarchy(hier_file)
 
     srv.allow_reuse_address = True
     print >> sys.stderr, "Serving on %s:%d... :)" % (host, port)
--- ml/fromscratch/testml.py
+++ ml/fromscratch/testml.py
@@ -27,7 +27,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import SGDClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier
 # from sklearn import svm
+
+from sklearn import metrics
 
 import nltk.stem
 
@@ -48,6 +51,7 @@ class Classifier:
         self.cat_n = 0
 
         self.vectorizer = StemmedTfidfVectorizer(min_df=1, ngram_range=(1,2))
+        # self.vectorizer = TfidfVectorizer(min_df=1, analyzer='char', ngram_range=(2,4))
         self.classifier = None
         self.loaded_docs = 0
 
@@ -71,7 +75,7 @@ class Classifier:
             title = rec['title']
             desc = rec['desc']
 
-            c = cat_name
+            c = sub_cat_name
             cat_id = self.cats.get(c, None)
 
             if cat_id == None:
@@ -94,9 +98,17 @@ class Classifier:
         print >> sys.stderr, "Learning..."
         train_features = self.vectorizer.fit_transform(train_docs)
 
-        # self.classifier = svm.SVC() # modified_huber
-        self.classifier = SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=5, random_state=42)
-        # self.classifier = DecisionTreeClassifier()
+        # self.classifier = svm.SVC() # modified_huber | hinge
+        # self.classifier = SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=5, random_state=42)
+        
+        #base_dc  = DecisionTreeClassifier(max_depth=1, min_samples_leaf=1)
+        #base_sgd = SGDClassifier(loss='modified_huber', penalty='l2', alpha=1e-3, n_iter=5, random_state=42)
+        # base_sgd = SGDClassifier(loss='modified_huber', penalty='l2', alpha=5e-4, n_iter=100, power_t=0.5, random_state=12, shuffle=False, warm_start=False)
+        # base2_ada = AdaBoostClassifier(base_estimator=base_dc, n_estimators=50, learning_rate=0.5, algorithm="SAMME")
+        # self.classifier = AdaBoostClassifier(base_estimator=base2_ada, n_estimators=100, learning_rate=0.5, algorithm="SAMME")
+
+        self.classifier = SGDClassifier(loss='modified_huber', penalty='l2', alpha=5e-4, n_iter=100, power_t=0.5, random_state=12, shuffle=False, warm_start=False)
+
         print >> sys.stderr, self.classifier
 
         self.classifier.fit(train_features, train_target)
@@ -115,11 +127,12 @@ class Classifier:
 if __name__ == '__main__':
     # parse argv
     if len(sys.argv) < 2:
-        print >> sys.stderr, "Usage:\n  %s <scan_json_file> [keyboard]" % sys.argv[0]
+        print >> sys.stderr, "Usage:\n  %s <scan_json_file> [keyboard|test_self]" % sys.argv[0]
         sys.exit(1)
 
     json_file = sys.argv[1]
     test_from_keyboard = (len(sys.argv) > 2 and sys.argv[2] == 'keyboard')
+    test_self          = (len(sys.argv) > 2 and sys.argv[2] == 'test_self')
 
     cls = Classifier()
     (docs, cats) = cls.load_data(json_file)
@@ -127,14 +140,14 @@ if __name__ == '__main__':
 
     # перемешиваем выборку и подготавливаем из неё обучающее и тестовое множества
     data = [(d, c) for (d, c) in zip(docs, cats)]
-    random.seed()
+    random.seed(1)
     random.shuffle( data )
 
-    if test_from_keyboard:
+    if test_from_keyboard or test_self:
         train_data = data
         test_data = []
     else:
-        r = len(data)*2/3
+        r = len(data)*9/10
         train_data = data[:r]
         test_data = data[r:]
 
@@ -145,6 +158,8 @@ if __name__ == '__main__':
 
     cls.train(train_docs, train_target)
 
+    cat_names = [cls.cat_id2name(x) for x in xrange(cls.cat_n)]
+
     # test
     if test_from_keyboard:
         # predict from keyboard
@@ -153,6 +168,25 @@ if __name__ == '__main__':
             line = raw_input()
             cat_id = cls.predict(line)
             print "\t%d=%s" % (cat_id, cls.cat_id2name(cat_id))
+    elif test_self:
+        predicted = []
+
+        for i in xrange( len(train_docs) ):
+            d = train_docs[i]
+            t = train_target[i]
+
+            cat_id = cls.predict(d)
+
+            ok = "OK" if t == cat_id else "BAD";
+            
+            prn = u"%s\t%s\t%s\t%s" % (ok, d, cls.cat_id2name(t), cls.cat_id2name(cat_id))
+            print prn.encode('utf-8')
+
+            predicted.append( cat_id )
+        
+        s = metrics.classification_report(train_target, predicted, target_names=cat_names)
+        print >> sys.stderr
+        print >> sys.stderr, s.encode('utf-8')
     else:
         # predict from the same collection
         print >> sys.stderr, "Testing..."
@@ -178,11 +212,6 @@ if __name__ == '__main__':
             test_predicted.append( cat_id )
 
         # show metrics
-        # accuracy = float(eq) / float(len(test_docs))
-        # print "Total: %d, equal: %d, accuracy: %f" % (len(test_docs), eq, accuracy)
-        from sklearn import metrics
-
-        cat_names = [cls.cat_id2name(x) for x in xrange(cls.cat_n)]
         s = metrics.classification_report(test_target, test_predicted, target_names=cat_names)
         print
         print s.encode('utf-8')
